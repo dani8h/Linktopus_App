fdcecc1dcb226d31a25ddb6461af9f90f44f81b7
Sowmesh <sowmeshshailesh1234@gmail.com>
2023-04-16 23:43:40 +0530

    ProfilePgAdded

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:linktopus_app/profile.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homepage(),
+      home: Profile(),
     );
   }
 }
